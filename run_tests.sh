b10e700f53b15572e5dd618bccb206e23dbd53df
Tony Simonutti <anthonysimonutti@users.noreply.github.com>
2018-02-28 01:01:36 -0500

    Fixed where we do the pyflakes and the pep8

--- run_tests.sh
+++ run_tests.sh
@@ -7,9 +7,9 @@ VENV=".venv-$BUILD_NUMBER"
 virtualenv "$VENV"
 source "$VENV/bin/activate"
 
+pep8 grabber > pep8.log || true
+pyflakes grabber > pyflakes.log || true
+
 pip install -e .
 
 PYTHONPATH=. python -m grabber/tests/run_tests
-
-pep8 grabber > pep8.log || true
-pyflakes grabber > pyflakes.log || true
