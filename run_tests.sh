af199d83b9da71d73f54b3e77d1bb448e7df3f67
Tony Simonutti <anthonysimonutti@users.noreply.github.com>
2018-02-28 00:55:56 -0500

    Added .log files to gitignore

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+set -x
 
 # Create a new virtual environment for the build
 VENV=".venv-$BUILD_NUMBER"
@@ -8,4 +9,7 @@ source "$VENV/bin/activate"
 
 pip install -e .
 
-PYTHONPATH=. python -m tests/run_tests 
+PYTHONPATH=. python -m grabber/tests/run_tests
+
+pep8 grabber > pep8.log || True
+pyflakes grabber > pyflakes.log || True
